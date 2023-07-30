4ea6f80508572db2a157de92cd9206268512523c
aboura <bouchrabichou638@gmail.com>
2023-07-30 23:24:31 +0200

    completedassignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name:"moloud",
+    streetAddress:"Ain-elbey",
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updatedEmployee = {
+      ...employee, 
+      [key]: value, 
+    };
+
+    return updatedEmployee;
+  }
+  
+  
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,key,value) {
+  employee [key] = value;
+return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+  const updatedEmployee = { ...employee };
+  delete updatedEmployee[key];
+  return updatedEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee,key) {
+  delete employee [key];
+  return employee;
+}
